76f4260a739e6883a2515443aaff3a8053f2b6aa
Michael Johns <michael.johns2@gmail.com>
2016-08-19 19:01:23 -0400

    adjusted data containers, separated core,accumulo, and stack yml, updated README

--- conf_scripts/create_data_containers.sh
+++ conf_scripts/create_data_containers.sh
@@ -19,25 +19,25 @@ function continueOrQuit {
 
 PULL_PREFIX="${1:-$PULL_PREFIX}"
 
-#name of data container for zookeeper
-DATA_CONTAINER_ZOOKEEPER="${2:-$DATA_CONTAINER_ZOOKEEPER}"
-
 #name of data container for hadoop-name
-DATA_CONTAINER_HADOOP_NAME="${3:-$DATA_CONTAINER_HADOOP_NAME}"
+DATA_CONTAINER_HADOOP_NAME="${2:-$DATA_CONTAINER_HADOOP_NAME}"
 
 #name of data container for hadoop-sname
-DATA_CONTAINER_HADOOP_SNAME="${4:-$DATA_CONTAINER_HADOOP_SNAME}"
+DATA_CONTAINER_HADOOP_SNAME="${3:-$DATA_CONTAINER_HADOOP_SNAME}"
 
 #name of data container for hadoop-data
-DATA_CONTAINER_HADOOP="${5:-$DATA_CONTAINER_HADOOP}"
+DATA_CONTAINER_HADOOP="${4:-$DATA_CONTAINER_HADOOP}"
+
+#name of data container for accumulo (master)
+DATA_CONTAINER_ACCUMULO="${5:-$DATA_CONTAINER_ACCUMULO}"
 
 ## VERIFY OPERATION
 echo "... USING THE FOLLOWING VARIABLES (SHOULD NOT BE EMPTY! YOU CAN PROVIDE VALUES AS ARGS):"
 echo "ARG1 | PULL_PREFIX: '$PULL_PREFIX'"
-echo "ARG2 | DATA_CONTAINER_ZOOKEEPER: '$DATA_CONTAINER_ZOOKEEPER'"
-echo "ARG3 | DATA_CONTAINER_HADOOP_NAME: '$DATA_CONTAINER_HADOOP_NAME'"
-echo "ARG4 | DATA_CONTAINER_HADOOP_SNAME: '$DATA_CONTAINER_HADOOP_SNAME'"
-echo "ARG5 | DATA_CONTAINER_HADOOP: '$DATA_CONTAINER_HADOOP'"
+echo "ARG2 | DATA_CONTAINER_HADOOP_NAME: '$DATA_CONTAINER_HADOOP_NAME'"
+echo "ARG3 | DATA_CONTAINER_HADOOP_SNAME: '$DATA_CONTAINER_HADOOP_SNAME'"
+echo "ARG4 | DATA_CONTAINER_HADOOP: '$DATA_CONTAINER_HADOOP'"
+echo "ARG5 | DATA_CONTAINER_ACCUMULO: '$DATA_CONTAINER_ACCUMULO'"
 continueOrQuit ''
 
 ## ON CONTINUE
@@ -45,10 +45,10 @@ continueOrQuit ''
 # the data containers will be used with volumes_from directive in docker-compose-dev.yml
 # remember, network mode is none if using compose, e.g. `network_mode: none`
 
-docker create --name ${DATA_CONTAINER_ZOOKEEPER} ${PULL_PREFIX}/geodocker-zookeeper /bin/true
-docker create --name ${DATA_CONTAINER_HADOOP_NAME} ${PULL_PREFIX}/geodocker-hadoop /bin/true
+docker create -it --name ${DATA_CONTAINER_HADOOP_NAME} ${PULL_PREFIX}/geodocker-hadoop /bin/true
 docker create -it --name ${DATA_CONTAINER_HADOOP_SNAME} ${PULL_PREFIX}/geodocker-hadoop /bin/true
 docker create -it --name ${DATA_CONTAINER_HADOOP} ${PULL_PREFIX}/geodocker-hadoop /bin/true
+docker create -it --name ${DATA_CONTAINER_ACCUMULO} ${PULL_PREFIX}/geodocker-accumulo /bin/true
 
 echo "...expect to see the new data containers 'created' (not running)"
 echo "do you see them in 'docker ps -a'?"
--- conf_scripts/remove_data_containers.sh
+++ conf_scripts/remove_data_containers.sh
@@ -17,34 +17,34 @@ function continueOrQuit {
     esac	       
 } 
 
-#name of data container for zookeeper
-DATA_CONTAINER_ZOOKEEPER="${1:-$DATA_CONTAINER_ZOOKEEPER}"
-
 #name of data container for hadoop-name
-DATA_CONTAINER_HADOOP_NAME="${2:-$DATA_CONTAINER_HADOOP_NAME}"
+DATA_CONTAINER_HADOOP_NAME="${1:-$DATA_CONTAINER_HADOOP_NAME}"
 
 #name of data container for hadoop-sname
-DATA_CONTAINER_HADOOP_SNAME="${3:-$DATA_CONTAINER_HADOOP_SNAME}"
+DATA_CONTAINER_HADOOP_SNAME="${2:-$DATA_CONTAINER_HADOOP_SNAME}"
 
 #name of data container for hadoop-data
-DATA_CONTAINER_HADOOP="${4:-$DATA_CONTAINER_HADOOP}"
+DATA_CONTAINER_HADOOP="${3:-$DATA_CONTAINER_HADOOP}"
+
+#name of data container for accumulo (master)
+DATA_CONTAINER_ACCUMULO="${4:-$DATA_CONTAINER_ACCUMULO}"
 
 ## VERIFY OPERATION
 echo "... USING THE FOLLOWING VARIABLES (SHOULD NOT BE EMPTY! YOU CAN PROVIDE VALUES AS ARGS):"
-echo "ARG1 | DATA_CONTAINER_ZOOKEEPER: '$DATA_CONTAINER_ZOOKEEPER'"
-echo "ARG2 | DATA_CONTAINER_HADOOP_NAME: '$DATA_CONTAINER_HADOOP_NAME'"
-echo "ARG3 | DATA_CONTAINER_HADOOP_SNAME: '$DATA_CONTAINER_HADOOP_SNAME'"
-echo "ARG4 | DATA_CONTAINER_HADOOP: '$DATA_CONTAINER_HADOOP'"
+echo "ARG1 | DATA_CONTAINER_HADOOP_NAME: '$DATA_CONTAINER_HADOOP_NAME'"
+echo "ARG2 | DATA_CONTAINER_HADOOP_SNAME: '$DATA_CONTAINER_HADOOP_SNAME'"
+echo "ARG3 | DATA_CONTAINER_HADOOP: '$DATA_CONTAINER_HADOOP'"
+echo "ARG4 | DATA_CONTAINER_ACCUMULO: '$DATA_CONTAINER_ACCUMULO'"
 continueOrQuit ''
 
 ## ON CONTINUE
 # this script will remove the data containers for hadoop
 # the data containers are used with volumes_from directive in docker-compose-dev.yml
 
-docker rm ${DATA_CONTAINER_ZOOKEEPER}
 docker rm ${DATA_CONTAINER_HADOOP_NAME}
 docker rm ${DATA_CONTAINER_HADOOP_SNAME}
 docker rm ${DATA_CONTAINER_HADOOP}
+docker rm ${DATA_CONTAINER_ACCUMULO}
 
 echo "...expect to not see the new data containers"
 echo "do you see them in 'docker ps -a'?"
